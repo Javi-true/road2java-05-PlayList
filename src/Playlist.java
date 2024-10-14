ed484bb5428146959028d5defb79a014883453f8
Javier Trujillo <rivera.dev@outlook.com>
2024-10-14 03:18:15 -0600

    Added - array favoriteSongs

--- src/Playlist.java
+++ src/Playlist.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 public class Playlist {
     public static void main(String[] args){
 
+        String[] favoriteSongs = {"Alex - la uno", "Alex - la dos","Alex - la tres","Alex - la cuatro","Alex - la cinco","Alex - la seis","Alex - la siete","Alex - la ocho","Alex - la nueve","Alex - la diez"};
+        
     }
 }
