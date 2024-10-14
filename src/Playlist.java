c7e7780b9bf3072660d7c58fa7131f67418f3477
Javier Trujillo <rivera.dev@outlook.com>
2024-10-14 03:20:40 -0600

    Added - arrayList desetIslandPlaylist

--- src/Playlist.java
+++ src/Playlist.java
@@ -4,6 +4,12 @@ public class Playlist {
     public static void main(String[] args){
 
         String[] favoriteSongs = {"Alex - la uno", "Alex - la dos","Alex - la tres","Alex - la cuatro","Alex - la cinco","Alex - la seis","Alex - la siete","Alex - la ocho","Alex - la nueve","Alex - la diez"};
+
+        System.out.println(favoriteSongs[0]);
+        System.out.println(favoriteSongs[1]);
+        System.out.println(favoriteSongs[2]);
+
+        ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
         
     }
 }
