e28653bf5ae952a1924db517187cc6b12229b21b
Javier Trujillo <rivera.dev@outlook.com>
2024-10-14 03:27:18 -0600

    Added - combined array and arrayList

--- src/Playlist.java
+++ src/Playlist.java
@@ -10,6 +10,18 @@ public class Playlist {
         System.out.println(favoriteSongs[2]);
 
         ArrayList<String> desertIslandPlaylist = new ArrayList<String>();
-        
+
+        desertIslandPlaylist.add("Nina - popo");
+        desertIslandPlaylist.add("Nina - pipi");
+        desertIslandPlaylist.add("Nina - moloca");
+        desertIslandPlaylist.add("Nina - el gas");
+        desertIslandPlaylist.add("Nina - churrito");
+
+        System.out.println(desertIslandPlaylist);
+
+
+        desertIslandPlaylist.addAll(Arrays.asList(favoriteSongs));
+        System.out.println(desertIslandPlaylist);
+        System.out.println(desertIslandPlaylist.size());
     }
 }
